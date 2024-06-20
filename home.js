70a8ac966da0699afc9a91a76945de5d9ccf8a0f
Omar J <100746749+omsinj@users.noreply.github.com>
2024-06-20 22:06:15 +0100

    Create home.js

---
+++ home.js
@@ -0,0 +1,14 @@
+// src/components/Home.js
+
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Educational Platform for DEI</h1>
+      <p>Embark on a cultural journey and expand your knowledge.</p>
+    </div>
+  );
+}
+
+export default Home;
