3b61fc6d1bd6765f8a8f4f81ff0eb658ffffb455
Omar J <100746749+omsinj@users.noreply.github.com>
2024-06-20 21:56:30 +0100

    Update urls.py

--- cultural_journey/cultural_journey/urls.py
+++ cultural_journey/cultural_journey/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+# trivia/urls.py
+
+from .views import QuestionList, ProgressDetail, UserList, ResourceList
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('questions/', QuestionList.as_view(), name='question-list'),
+    path('progress/<int:pk>/', ProgressDetail.as_view(), name='progress-detail'),
+    path('users/', UserList.as_view(), name='user-list'),
+    path('resources/', ResourceList.as_view(), name='resource-list'),
 ]
