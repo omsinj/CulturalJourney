a52092b37c5bd24900ae8dee43f681baeaf51056
Omar J <100746749+omsinj@users.noreply.github.com>
2024-06-20 21:55:38 +0100

    Update views.py

--- cultural_journey/trivia/views.py
+++ cultural_journey/trivia/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
-# Create your views here.
+# trivia/views.py
+
+from rest_framework import generics
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from .models import Question, Progress, Resource
+from .serializers import QuestionSerializer, ProgressSerializer, UserSerializer, ResourceSerializer
+from .recommendation import recommend_questions
+
+class QuestionList(generics.ListCreateAPIView):
+    serializer_class = QuestionSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return recommend_questions(user)
+
+class ProgressDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Progress.objects.all()
+    serializer_class = ProgressSerializer
+
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class ResourceList(generics.ListCreateAPIView):
+    queryset = Resource.objects.all()
+    serializer_class = ResourceSerializer
+
