20e169e0df3918ab8bc298110daea55ef4e09a8f
Omar J <100746749+omsinj@users.noreply.github.com>
2024-06-20 22:10:07 +0100

    Update views.py

--- cultural_journey/trivia/views.py
+++ cultural_journey/trivia/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # trivia/views.py
 
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 from .models import Question, Progress, Resource
@@ -16,15 +16,22 @@ class QuestionList(generics.ListCreateAPIView):
         user = self.request.user
         return recommend_questions(user)
 
+    def create(self, request, *args, **kwargs):
+        user = request.user
+        questions = recommend_questions(user)
+        return Response(QuestionSerializer(questions, many=True).data)
+
 class ProgressDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Progress.objects.all()
     serializer_class = ProgressSerializer
 
-class UserList(generics.ListCreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-class ResourceList(generics.ListCreateAPIView):
-    queryset = Resource.objects.all()
-    serializer_class = ResourceSerializer
-
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.correct_answers += 1
+        if instance.correct_answers >= LEVEL_QUESTIONS_COUNT[instance.current_level]:
+            instance.correct_answers = 0
+            next_level_index = LEVELS.index(instance.current_level) + 1
+            if next_level_index < len(LEVELS):
+                instance.current_level = LEVELS[next_level_index]
+        instance.save()
+        return Response(ProgressSerializer(instance).data)
