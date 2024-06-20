72ae65572156a3fd31ca07d431748537cfff8bc8
Omar J <100746749+omsinj@users.noreply.github.com>
2024-06-20 22:23:19 +0100

    Update settings.py

--- cultural_journey/cultural_journey/settings.py
+++ cultural_journey/cultural_journey/settings.py
@@ -10,7 +10,27 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-# cultural_journey/settings.py
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure--$%979_-18)e0#(wu2v3dw7f8$!1&j3u-82a$9au+xh7t9u07b'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+I# cultural_journey/settings.py
 
 INSTALLED_APPS = [
     # ... other installed apps
